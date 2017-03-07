ffbb31967740404efb921f9155e403b348e9f99d
Ændrew Rininsland <andrew.rininsland@ft.com>
2017-03-07 20:27:44 +0000

    Adding to config

--- CONFIG.js
+++ CONFIG.js
@@ -6,6 +6,21 @@
 
 const CONFIG = {
   'page title': 'Example Smarter Kit page',
+  'canonical url': 'https://',
+  'main image url': 'https://',
+
+  // These will fallback to the above values if left undefined.
+  'twitter url': undefined,
+  'facebook url': undefined,
+  'twitter image url': undefined,
+  'facebook image url': undefined,
+  'twitter card': 'summary_large_image', // Other option is just 'summary'; @see https://dev.twitter.com/cards/overview
+  'twitter creator value': undefined,
+
+  // Flags
+  'flag:analytics': true,
+  'tracking:product': null,
+  'tracking:microsite': null,
 };
 
 module.exports = CONFIG;
